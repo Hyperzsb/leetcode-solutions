9d33ece5b096bfb6eb74d21ca5649ffa766645f7
Shaobo ZHANG <hyperzsb@qq.com>
2022-11-07 17:46:04 +0800

    feat(solution): add the solution in c to the problem 35
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ 35-Search-Insert-Position/solution.c
@@ -0,0 +1,21 @@
+int searchInsert(int* nums, int numsSize, int target){
+    int start = 0, end = numsSize - 1, half = (start + end) / 2;
+    if (target <= nums[start])
+        return start;
+    if (target > nums[end])
+        return end + 1;
+    if (target == nums[end])
+        return end;
+    while (start < end) {
+        if (target == nums[half]) {
+            return half;
+        } else if (target < nums[half]) {
+            end = half - 1;
+        } else {
+            start = half + 1;
+        }
+        half = (start + end) / 2;
+    }
+    return start;
+}
+
