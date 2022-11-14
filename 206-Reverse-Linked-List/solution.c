95f7ea12567571b3bcf554f0d21bb205883204ac
Shaobo ZHANG <hyperzsb@qq.com>
2022-11-14 11:26:44 +0800

    feat(solution): add the solution in c to the problem 206
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ 206-Reverse-Linked-List/solution.c
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode **ptrList, *retHead = head, *current = head;
+    int len = 0, index = 0;
+    while(current) {
+        len++;
+        current = current->next;
+    }
+    
+    if (len <= 1) {
+        return head;
+    }
+    
+    ptrList = (struct ListNode **)malloc(len * sizeof(struct ListNode *));
+    current = head;
+    
+    while(current) {
+        ptrList[index++] = current;
+        current = current->next;
+    }
+    
+    retHead = ptrList[len - 1];
+    current = retHead;
+    for (int i = len - 2; i >= 0; i--) {
+        current->next = ptrList[i];
+        current = current->next;
+    }
+    current->next = NULL;
+    
+    return retHead;
+}
+
