a3f82e940f182d034aef091eb000640acd6659df
Shaobo ZHANG <hyperzsb@qq.com>
2022-11-19 15:08:02 +0800

    feat(ctl): implement the update function of the ctl utility
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- ctl.sh
+++ ctl.sh
@@ -27,7 +27,7 @@ help() {
 create() {
     if [ -d "problems/$2" ]; then
         echo -e "The problem \"$2\" already exists. If you want to update it, try:\n"
-        echo -e "\t./ctl.sh update --$1 $2"
+        echo -e "\t ./ctl.sh update --$1 $2"
 
         return
     fi
@@ -36,7 +36,7 @@ create() {
     touch "problems/$2/solution.$1"
 
     vim "problems/$2/solution.$1"
-    echo "Problem $2 solution in $1 language created."
+    echo -e "The new solution of the problem $2 in $1 programming language created.\n"
 
     IFS='-'
     read -ra split_string <<< "$2"
@@ -44,11 +44,27 @@ create() {
     git add .
     git commit -S -s -m "feat(solution): add the solution in $1 to the problem ${split_string[0]}"
 
-    echo "Problem $2 solution in $1 language committed."
+    echo -e "\nThe new solution of the problem $2 in $1 programming language committed."
 }
 
 update() {
-    echo "This feature has not been implemented yet."
+    if [ ! -d "problems/$2" ]; then
+        echo -e "The problem \"$2\" does not exist. If you want to create it, try:\n"
+        echo -e "\t ./ctl.sh create --$1 $2"
+
+        return
+    fi
+
+    vim "problems/$2/solution.$1"
+    echo -e "The solution of the problem $2 in $1 programming language updated\n"
+
+    IFS='-'
+    read -ra split_string <<< "$2"
+
+    git add .
+    git commit -S -s -m "feat(solution): update the solution in $1 to the problem ${split_string[0]}"
+
+    echo -e "\nThe solution of the problem $2 in $1 programming language committed"
 }
 
 info() {
@@ -60,7 +76,7 @@ shift
 
 case "$command" in
 create)
-    language="c"
+    language=""
 
     opt="$1"
     shift
@@ -81,7 +97,7 @@ create)
         language="py"
         ;;
     *)
-        help
+        echo -e "Invalid option. See './ctl.sh help'"
         exit 1
         ;;
     esac
@@ -89,16 +105,50 @@ create)
     name="$1"
     shift
     if [ -z "$name" ]; then
-        echo -e "Problem name required\n"
+        echo -e "The problem name is required\n"
         help
-        exit 1
+        exit 2
     fi
 
     create "$language" "$name"
     ;;
 
 update)
-    update
+    language=""
+
+    opt="$1"
+    shift
+    case "$opt" in
+    --c)
+        language="c"
+        ;;
+    --cpp)
+        language="cpp"
+        ;;
+    --go)
+        language="go"
+        ;;
+    --js)
+        language="js"
+        ;;
+    --py)
+        language="py"
+        ;;
+    *)
+        echo -e "Invalid option. See './ctl.sh help'"
+        exit 1
+        ;;
+    esac
+
+    name="$1"
+    shift
+    if [ -z "$name" ]; then
+        echo -e "The problem name is required\n"
+        help
+        exit 2
+    fi
+
+    update "$language" "$name"
     ;;
 
 info)
