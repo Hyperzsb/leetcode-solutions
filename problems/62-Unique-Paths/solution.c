87019a5163d0dd515df96750c237d506099793c1
Shaobo ZHANG <hyperzsb@qq.com>
2022-11-21 17:15:57 +0800

    feat(solution): add the solution in c to the problem 62
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ problems/62-Unique-Paths/solution.c
@@ -0,0 +1,25 @@
+int uniquePaths(int m, int n){
+    int dp[102][102];
+    
+    dp[m - 1][n - 1] = 1;
+    
+    for(int i = m - 1; i >= 0; i--) {
+        for(int j = n -1; j >= 0; j--) {
+            if(i == m - 1 && j == n - 1) {
+                continue;
+            }
+            
+            dp[i][j] = 0;
+            
+            if(i < m - 1) {
+                dp[i][j] += dp[i + 1][j];
+            }
+            if(j < n - 1) {
+                dp[i][j] += dp[i][j + 1];
+            }
+        }
+    }
+    
+    return dp[0][0];
+}
+
