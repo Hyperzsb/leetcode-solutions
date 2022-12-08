a02cadee062dd83b7a859680f3d6888ee7eac47b
Shaobo ZHANG <hyperzsb@qq.com>
2022-12-08 17:18:22 +0800

    feat(solution): add the solution in go to the problem 94
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ problems/94-Binary-Tree-Inorder-Traversal/solution.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	left := inorderTraversal(root.Left)
+	right := inorderTraversal(root.Right)
+
+	sum := append(left, root.Val)
+	sum = append(sum, right...)
+
+	return sum
+}
+
