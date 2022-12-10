d8b12d229d2d6356d55401fe54b22d8c05e61319
Shaobo ZHANG <hyperzsb@qq.com>
2022-12-10 20:50:36 +0800

    feat(solution): add the solution in go to the problem 118
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ problems/118-Pascall's-Triangle/solution.go
@@ -0,0 +1,19 @@
+func generate(numRows int) [][]int {
+	result := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		result[i] = make([]int, i+1)
+		result[i][0] = 1
+		result[i][i] = 1
+
+		if i == 1 {
+			continue
+		} else {
+			for j := 1; j < i; j++ {
+				result[i][j] = result[i-1][j-1] + result[i-1][j]
+			}
+		}
+	}
+
+	return result
+}
+
