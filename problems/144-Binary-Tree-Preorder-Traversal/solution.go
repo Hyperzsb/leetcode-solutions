34f0175930ed2a9c35435e903a9266fdc902f0ed
Shaobo ZHANG <hyperzsb@qq.com>
2022-12-11 21:28:28 +0800

    feat(solution): add the solution in go to the problem 144
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ problems/144-Binary-Tree-Preorder-Traversal/solution.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+	result := make([]int, 0)
+
+	if root == nil {
+		return result
+	}
+
+	result = append(result, root.Val)
+	result = append(result, preorderTraversal(root.Left)...)
+	result = append(result, preorderTraversal(root.Right)...)
+
+	return result
+}
+
