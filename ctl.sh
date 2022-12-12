9d16a9e646b340253948e5bf2fd99646162d7d9f
Shaobo ZHANG <hyperzsb@qq.com>
2022-12-12 10:49:50 +0800

    feat(ctl): add a discard option for the create func, and add some comments
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- ctl.sh
+++ ctl.sh
@@ -24,26 +24,39 @@ help() {
     echo -e "\t ./ctl.sh info 123\n"
 }
 
+# Create function is used to create a new solution of a new problem,
+# if and only if there is no previous problem with the same name as provided
 create() {
+    # Check whether there exists a problem with the same name as provided
+    # If so, print the help message telling user yo use the `update` command instead, and exit
     if [ -d "problems/$2" ]; then
         echo -e "The problem \"$2\" already exists. If you want to update it, try:\n"
         echo -e "\t ./ctl.sh update --$1 $2"
-
         return
     fi
 
+    # Create a new directory for the problem
     mkdir "problems/$2"
+    # Create a new file for the solution
     touch "problems/$2/solution.$1"
-
+    # Use Vim to edit the solution file
     vim "problems/$2/solution.$1"
     echo -e "The new solution of the problem $2 in $1 programming language created.\n"
 
-    IFS='-'
-    read -ra split_string <<< "$2"
+    # Prompt a message to receive the confirmation of user whether to commit the new problem
+    read -p "Do you want to commit this new solution? (y/n, default y) " confirm
+    # If user does not confirm, discard all changes, and exit
+    if [[ "$confirm" == "n" ]]; then
+        rm -rf "problems/$2"
+        echo -e "\nThe new solution of the problem $2 in $1 programming language removed."
+        return
+    fi
 
+    # Extract the id of the problem
+    IFS='-' read -ra split_string <<< "$2"
+    # Add and commit the problem to Git
     git add .
     git commit -S -s -m "feat(solution): add the solution in $1 to the problem ${split_string[0]}"
-
     echo -e "\nThe new solution of the problem $2 in $1 programming language committed."
 }
 
