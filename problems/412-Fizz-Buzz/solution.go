248ef6be3feb8a16347f4fa9c514969399bd3984
Shaobo ZHANG <hyperzsb@qq.com>
2022-12-20 11:51:01 +0800

    feat(solution): add the solution in go to the problem 412
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ problems/412-Fizz-Buzz/solution.go
@@ -0,0 +1,17 @@
+func fizzBuzz(n int) []string {
+	result := make([]string, n)
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			result[i-1] = "FizzBuzz"
+		} else if i%3 == 0 {
+			result[i-1] = "Fizz"
+		} else if i%5 == 0 {
+			result[i-1] = "Buzz"
+		} else {
+			result[i-1] = fmt.Sprintf("%d", i)
+		}
+	}
+
+	return result
+}
+
