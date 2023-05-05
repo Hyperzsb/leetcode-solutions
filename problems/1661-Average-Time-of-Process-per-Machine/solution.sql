b29a6de6449bdefcde2805bbc8433111bcea4fa4
Shaobo ZHANG <hyperzsb@qq.com>
2023-05-05 10:51:43 +0800

    feat(solution): add the solution in sql to the problem 1661
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ problems/1661-Average-Time-of-Process-per-Machine/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select A1.machine_id, ROUND(AVG(A2.timestamp - A1.timestamp), 3) as processing_time 
+from Activity as A1
+inner join Activity as A2
+on A1.machine_id = A2.machine_id and A1.process_id = A2.process_id and A1.activity_type = 'start' and A2.activity_type = 'end'
+group by A1.machine_id;
+
