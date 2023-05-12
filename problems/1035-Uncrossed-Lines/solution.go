0defdb62797ec31fa1292f39483ceea0472fbc10
Shaobo ZHANG <hyperzsb@qq.com>
2023-05-12 08:50:41 +0800

    feat(solution): add the solution in go to the problem 1035
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ problems/1035-Uncrossed-Lines/solution.go
@@ -0,0 +1,27 @@
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func maxUncrossedLines(nums1 []int, nums2 []int) int {
+	dp := make([][]int, len(nums1)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(nums2)+1)
+	}
+
+	for i := 1; i <= len(nums1); i++ {
+		for j := 1; j <= len(nums2); j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[len(nums1)][len(nums2)]
+}
+
