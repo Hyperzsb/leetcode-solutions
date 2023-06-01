27e91c8108087397eb8c0460d8e327ab174d19f1
Shaobo ZHANG <hyperzsb@qq.com>
2023-06-01 09:03:15 +0800

    feat(solution): add the solution in java to the problem 709
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ problems/709-To-Lower-Case/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String toLowerCase(String s) {
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'A' && chars[i] <= 'Z') {
+                chars[i] = (char)(chars[i] - 'A' + 'a');
+            }
+        }
+
+        return new String(chars);
+    }
+}
+
